f3b6f5ec504f71943735ebd49705a0889a6b2132
zofiagrodecka <69384237+zofiagrodecka@users.noreply.github.com>
2021-04-13 18:27:59 +0200

    Janek update

--- src/segmentation.py
+++ src/segmentation.py
@@ -9,23 +9,25 @@ from copy import deepcopy
 from itertools import chain
 from PIL import Image
 
+BLUR_PARAM = 41
+
 
 def converting_to_gray_scale(file_name, show=False):
-    # cwd = pathlib.Path().absolute()
-    # file_path = str(cwd) + "\\Photos\\" + file_name
+    cwd = pathlib.Path().absolute()
+    file_path = str(cwd) + "\\Photos\\" + file_name
     
-    image = cv.imread(file_name)
+    image = cv.imread(file_path)
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     
     if show:
-        cv.imshow('ORIGINAL', image)
+        # cv.imshow('ORIGINAL', image)
         cv.imshow('GRAY_SCALE', gray)
         
     return gray
 
 
 def gaussian_blurring(image, show=False):
-    gaussed = cv.GaussianBlur(image, (0,0), cv.BORDER_DEFAULT)
+    gaussed = cv.GaussianBlur(image, (BLUR_PARAM,BLUR_PARAM), cv.BORDER_DEFAULT)
 
     if show:
         cv.imshow('BLURRED', gaussed)
@@ -52,7 +54,7 @@ def split_into_tones(image, show=False, brackets=[]):
                 Tones[ intensity//51 ].append([x,y])
 
         if show:
-            print_by_tones(image, Tones)
+            print_by_tones(image, Tones, [0, 51, 102, 153, 204, 255])
 
         return Tones
 
@@ -69,21 +71,26 @@ def split_into_tones(image, show=False, brackets=[]):
                         break
 
         if show:
-            print_by_tones(image, Tones)
+            print_by_tones(image, Tones, division)
 
         return Tones
 
 
-def print_by_tones(image, Tones):
+def print_by_tones(image, Tones, division):
+    masks = []
     N = len(Tones)
     for i in range(N):
-            print(f"Tone {i+1}: {len(Tones[i])}")
+            # print(f"Tone {i+1}: {len(Tones[i])}")
             copy = deepcopy(image)
             for (x, y) in Tones[i]:
                 copy[x, y] = 255
 
-            copy_scaled = cv.resize(copy, None, fx=0.3, fy=0.3, interpolation = cv.INTER_LINEAR)
-            cv.imshow(f"Tone {i+1}", copy_scaled[:,:])
+            copy_scaled = cv.resize(copy, None, fx=0.2, fy=0.2, interpolation=cv.INTER_LINEAR)
+            cv.imshow(f"Tone {i + 1} - {division[i]}:{division[i + 1]}", copy_scaled[:, :])
+            masks.append(copy_scaled[:, :])
+
+    # All = np.concatenate(masks, axis=1)
+    # cv.imshow("Tonal sections", All)
 
 
 def apply_threshold(blurred_image, gray_image, Tones, show=False):
@@ -120,7 +127,7 @@ def change_threshold(image, new_value, show=False):
     return image_result
 
 
-def merge_pictures(results):
+def merge_pictures(results, show=False):
     n = len(results)
 
     if n < 2:
@@ -135,25 +142,46 @@ def merge_pictures(results):
         # cv.imshow(f"Tones {i+1} after merge", merged)
 
     image_result = Image.fromarray(merged.astype(np.uint8))
+
+    if show:
+        cv.imshow("Result", merged)
+
     return image_result
 
 
 if __name__ == "__main__":
-    
-    file_path = sys.argv[1]
+
+    # Getting input from usr
+    file_name = sys.argv[1]
     gray_scale_div = []
     for i in range(2, len(sys.argv)):
         parameter = sys.argv[i]
         gray_scale_div.append(int(parameter))
 
-    gray = converting_to_gray_scale(file_path, show=False)
+    # Converting image
+    gray = converting_to_gray_scale(file_name, show=True)
+
     gaussed = gaussian_blurring(gray, show=False)
+
     Tones = split_into_tones(gaussed, brackets=gray_scale_div, show=True)
 
     masked, thresholded = apply_threshold(gaussed, gray, Tones, show=False)
 
-    result = merge_pictures(thresholded)
-    result.save("result.tif")
+    result = merge_pictures(thresholded, show=True)
 
     cv.waitKey()
+
+    # Saving result image
+    usr_wants_to_save = "_"
+    while usr_wants_to_save.upper() != "Y" or usr_wants_to_save.upper() != "N":
+        usr_wants_to_save = input("Do you want to save the result? [Y/n]:\n> ")
+
+        if usr_wants_to_save.upper() == "Y":
+            result_file_name = input("Put result file name:\n> ")
+            result.save(f"Results/{result_file_name}")
+            break
+        elif usr_wants_to_save.upper() == "N":
+            break
+        else:
+            print("Wrong response character!")
  
