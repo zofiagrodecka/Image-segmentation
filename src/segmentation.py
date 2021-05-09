91682cbf3b5393e687b0c6c73c623578ac099d0a
Amalazas <57595793+Amalazas@users.noreply.github.com>
2021-05-09 14:59:25 +0200

    dodane wybieranie i zapisywanie z okna

--- src/segmentation.py
+++ src/segmentation.py
@@ -9,13 +9,20 @@ from copy import deepcopy
 from PIL import Image
 from numpy import ma
 from skimage.filters import threshold_otsu
+import tkinter.filedialog  #import askopenfilename
+from tkinter import Tk
+from os.path import abspath
 
 BLUR_PARAM = 0
 
 
-def converting_to_gray_scale(file_name, show=False):
-    cwd = pathlib.Path().absolute()
-    file_path = str(cwd) + "\\Photos\\" + file_name
+def converting_to_gray_scale(file_name, show=False, explore_files=False):
+
+    if explore_files:
+        file_path = file_name
+    else:
+        cwd = pathlib.Path().absolute()
+        file_path = str(cwd) + "\\Photos\\" + file_name
     
     image = cv.imread(file_path)
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
@@ -52,7 +59,10 @@ def split_into_tones(image, show=False, brackets=[]):
         for y in range(image.shape[1]):
             for x in range(image.shape[0]):
                 intensity = image[x, y]
-                Tones[intensity//51].append([x, y])
+                div = intensity//51
+                if div == 5:
+                    div -= 1
+                Tones[div].append([x, y])
 
         if show:
             print_by_tones(image, Tones, [0, 51, 102, 153, 204, 255])
@@ -99,7 +109,6 @@ def apply_threshold(blurred_image, gray_image, Tones, show=False):
     mask = np.zeros(blurred_image.shape[:2], np.uint8)
     masked = []
     results = []
-    threshold_values = []
 
     for i in range(N):
         for (x, y) in Tones[i]:
@@ -109,21 +118,20 @@ def apply_threshold(blurred_image, gray_image, Tones, show=False):
         thr_masked = ma.masked_array(masked_img, mask == 0)  # w masce z np. sa 1 tam gdzie sa 0 (czern) w mask
 
         if thr_masked.compressed().shape[0] != 0:  # Brute force rozwiązanie problemu xd
-            value = threshold_otsu(thr_masked.compressed())
-            threshold_values.append(value)
-            # print(f"tone: {i+1}, threshold: {threshold_value}")
-            threshold, image_result = cv.threshold(masked_img, value, 255, cv.THRESH_BINARY)
+            threshold_value = threshold_otsu(thr_masked.compressed())
+            threshold, image_result = cv.threshold(masked_img, threshold_value, 255, cv.THRESH_BINARY)
 
             masked.append(masked_img)
             results.append(image_result)
             if show:
-                # cv.imshow(f"Mask {i + 1}", mask)
+                cv.imshow(f"Mask {i + 1}", mask)
                 masked_img_scaled = cv.resize(masked_img, None, fx=0.6, fy=0.6, interpolation=cv.INTER_LINEAR)
                 cv.imshow(f"Masked image {i + 1}", masked_img_scaled)
-                image_result_scaled = cv.resize(image_result, None, fx=0.6, fy=0.6, interpolation=cv.INTER_LINEAR)
-                cv.imshow(f"Tone {i + 1} after threshold", image_result_scaled)
+                # image_result_scaled = cv.resize(image_result, None, fx=0.6, fy=0.6, interpolation=cv.INTER_LINEAR)
+                image = np.array(image_result)
+                cv.imshow(f"Tone {i + 1} after threshold", image)
             mask = np.zeros(blurred_image.shape[:2], np.uint8)
-    return masked, threshold_values, results
+    return masked, results
 
 
 def change_threshold(image, new_value, show=False):
@@ -159,41 +167,51 @@ def convert_from_array(image):
 
 if __name__ == "__main__":
 
-    # Getting input from usr
     if len(sys.argv) < 2:
-        print("This program needs arguments: Photo_FILEname(obligatory), [Thresholds](optional)")
+        sys.exit("This program needs arguments: Photo_FILEname(obligatory) or \"explore\", [Thresholds](optional)")
 
+    if sys.argv[1] == "explore":
+        Tk().withdraw()
+        file_name = tkinter.filedialog.askopenfilename()
     else:
         file_name = sys.argv[1]
-        gray_scale_div = []
-        for i in range(2, len(sys.argv)):
-            parameter = sys.argv[i]
-            gray_scale_div.append(int(parameter))
 
-        # Converting image
+    # Getting array of divisions
+    gray_scale_div = []
+    for i in range(2, len(sys.argv)):
+        parameter = sys.argv[i]
+        gray_scale_div.append(int(parameter))
+
+    # Converting image
+    if sys.argv[1] == "explore":
+        gray = converting_to_gray_scale(file_name, show=True, explore_files=True)
+    else:
         gray = converting_to_gray_scale(file_name, show=True)
 
-        gaussed = gaussian_blurring(gray, show=True)
+    gaussed = gaussian_blurring(gray, show=True)
 
-        Tones = split_into_tones(gaussed, brackets=gray_scale_div, show=False)
+    Tones = split_into_tones(gaussed, brackets=gray_scale_div, show=False)
 
-        masked, values, thresholded = apply_threshold(gaussed, gray, Tones, show=True)
+    masked, thresholded = apply_threshold(gaussed, gray, Tones, show=False)
 
-        result = merge_pictures(thresholded, show=True)
+    result = merge_pictures(thresholded, show=True)
 
-        cv.waitKey()
-        cv.destroyAllWindows()
+    cv.waitKey()
+    cv.destroyAllWindows()
 
-        # Saving result image
-        usr_wants_to_save = "_"
-        while usr_wants_to_save.upper() != "Y" or usr_wants_to_save.upper() != "N":
-            usr_wants_to_save = input("Do you want to save the result? [Y/n]:\n> ")
+    # Saving result image
+    usr_wants_to_save = "_"
+    while usr_wants_to_save.upper() != "Y" or usr_wants_to_save.upper() != "N":
+        usr_wants_to_save = input("Do you want to save the result? [Y/n]:\n> ")
 
-            if usr_wants_to_save.upper() == "Y":
-                result_file_name = input("Put result file name:\n> ")
-                cv.imwrite(f"Results/result_file_name", result, [cv.IMWRITE_PNG_BILEVEL, 1])
-                break
-            elif usr_wants_to_save.upper() == "N":
-                break
-            else:
-                print("Wrong response character!")
+        if usr_wants_to_save.upper() == "Y":
+            # result_file_name = input("Put result file name:\n> ")
+            # cv.imwrite(f"Results/{result_file_name}", result, [cv.IMWRITE_PNG_BILEVEL, 1])
+            f = tkinter.filedialog.asksaveasfile(mode='w', defaultextension='bmp')
+            cv.imwrite(f.name, result, [cv.IMWRITE_PNG_BILEVEL, 1])
+            f.close()
+            break
+        elif usr_wants_to_save.upper() == "N":
+            break
+        else:
+            print("Wrong response character!")
